9b2ff26fe1d6953ed407d8e790d1a2453a9944d6
zhaihaotian <zihan9198@gmail.com>
2024-02-08 16:09:02 +0800

    finished untill the first project(hog)

---
+++ lab/lab01/test.py
@@ -0,0 +1,12 @@
+def search(f):
+    x = 0
+    while True:
+        if f(x) :
+            return True
+        else :
+            return None
+        x += 1
+
+def check3(x):
+    return x == 3
+
