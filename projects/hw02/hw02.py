9f0d1bf5241e33cdbb8a2cdaff78e07d0dc68d92
zhaihaotian <zihan9198@gmail.com>
2024-02-08 19:10:41 +0800

    finished hw02

---
+++ projects/hw02/hw02.py
@@ -0,0 +1,180 @@
+HW_SOURCE_FILE=__file__
+
+
+def num_eights(x):
+    """Returns the number of times 8 appears as a digit of x.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    # print("x : " , x)
+    if x < 10 :
+        if x == 8 :
+            return 1
+        else :
+            return 0
+    else :
+        return num_eights(x // 10) + (1 if x % 10 == 8 else 0)
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+
+    "*** YOUR CODE HERE ***"
+    def calc(index , now_value , direction) :
+        if index == n :
+            return now_value
+        else :
+            if index % 8 == 0 or num_eights(index) :
+                return calc(index + 1 , now_value - direction , -direction)
+            else :
+                return calc(index + 1 , now_value + direction , direction)
+    return calc(1,1,1)
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(35578) # 4, 6
+    2
+    >>> missing_digits(12456) # 3
+    1
+    >>> missing_digits(16789) # 2, 3, 4, 5
+    4
+    >>> missing_digits(19) # 2, 3, 4, 5, 6, 7, 8
+    7
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10 :
+        return 0
+    else :
+        last_digit = n % 10
+        rest_digits = n // 10
+        second_last_digit = rest_digits % 10
+
+        # 计算最后两位之间缺失的数字数量
+        missing_between = max(0, last_digit - second_last_digit - 1)
+
+        # 递归地计算其余数字中的缺失数字数量
+        return missing_digits(rest_digits) + missing_between
+
+def next_largest_coin(coin):
+    """Return the next coin. 
+    >>> next_largest_coin(1)
+    5
+    >>> next_largest_coin(5)
+    10
+    >>> next_largest_coin(10)
+    25
+    >>> next_largest_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+
+def count_coins(total):
+    """Return the number of ways to make change for total using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])                                          
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    def helper(total, coin):
+        if total == 0:
+            return 1  # Found a valid combination
+        if total < 0 or coin is None:
+            return 0  # Not a valid combination
+        # Count ways including at least one of the current coin
+        with_coin = helper(total - coin, coin)
+        # Count ways excluding the current coin and moving to the next smaller denomination
+        without_coin = helper(total, next_largest_coin(coin))
+        return with_coin + without_coin
+
+    return helper(total, 1)  # Start with the smallest coin
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'
+
